af86f9414e15c058c94f7fd1020169678ea3f443
AdolfoMartinez <adolfomartinez@eprosima.com>
2020-01-29 09:16:56 +0100

    Refs #7409 Bugs fixed.

--- src/cpp/rtps/builtin/discovery/participant/PDPListener.cpp
+++ src/cpp/rtps/builtin/discovery/participant/PDPListener.cpp
@@ -130,6 +130,8 @@ void PDPListener::onNewCacheChangeAdded(
                     reader->getMutex().unlock();
                     lock.unlock();
 
+                    logInfo(RTPS_PDP_DISCOVERY, "New participant " << pdata->m_guid << " at " << "MTTLoc: " << pdata->metatraffic_locators << " DefLoc:" << pdata->default_locators);
+
                     parent_pdp_->announceParticipantState(false);
                     parent_pdp_->assignRemoteEndpoints(pdata);
                 }
@@ -140,6 +142,8 @@ void PDPListener::onNewCacheChangeAdded(
                 pdata->isAlive = true;
                 reader->getMutex().unlock();
                 lock.unlock();
+
+                logInfo(RTPS_PDP_DISCOVERY, "Update participant " << pdata->m_guid << " at " << "MTTLoc: " << pdata->metatraffic_locators << " DefLoc:" << pdata->default_locators);
 
                 if (parent_pdp_->updateInfoMatchesEDP())
                 {
